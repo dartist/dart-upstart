8d03967da7da563103c2bc711eccf55dcd282c97
Max Horstmann <max.horstmann@gmail.com>
2014-04-19 08:53:55 -0400

    add error handling to webhook

--- webhook.dart
+++ webhook.dart
@@ -6,15 +6,28 @@ import 'dart:convert';
        server.listen((HttpRequest request) {
          if (request.method=="POST") {
            print('Webhook triggered at ' + new DateTime.now().toString());
-           request.response.statusCode = 200;
-           Process.start('deploy.sh', []).then((process) {
-             process.stdout.transform(new Utf8Decoder())
-                           .transform(new LineSplitter())
-                           .listen((String line) => request.response.writeln(line));
-             process.exitCode.then((exitCode) {
-               request.response.writeln('Exit code = $exitCode');
-               request.response.close();
-             });
-           });
+           Process.start('bash', ['deploy.sh'])  // <-- point to deployment script
+             .then((process) {
+                 request.response.statusCode = 200;
+                 process.stdout.transform(new Utf8Decoder())
+                               .transform(new LineSplitter())
+                               .listen((String line) => request.response.writeln('stdout:  ' + line));
+                 process.stderr.transform(new Utf8Decoder())
+                               .transform(new LineSplitter())
+                               .listen((String line) => request.response.writeln('stderr:  ' + line));
+                 process.exitCode.then((exitCode) {
+                   request.response.writeln('Exit code = $exitCode');
+                   request.response.close();
+                 });
+               })
+             .catchError((e) {
+                 request.response.statusCode = 500;
+                 request.response.writeln('ERROR');
+                 request.response.writeln(e);
+                 request.response.close();
+               }); 
          }
- });});}
+		});
+	});
+	print('listing...');
+}
